db4b1533ecb5a4b95a3af636ebd0f319ed764f31
Jonathan Mathew <jonathanmathew@Jonathans-MBP.lan>
2024-02-21 12:29:14 -0500

    changes to deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"  # Specify your desired AWS region
 
   access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
+  secret_key = var.aws_secret_access_key #my key
 }
 
 resource "aws_instance" "example" {
