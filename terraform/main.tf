9f5cfaccdb2498e5b1b23c07f0e9859d7442fd26
Jonathan Mathew <jonathanmathew@Jonathans-MBP.lan>
2024-02-21 11:53:25 -0500

    changes to deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,8 @@
 provider "aws" {
   region = "us-east-1"  # Specify your desired AWS region
+
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
 
 resource "aws_instance" "example" {
@@ -31,7 +34,7 @@ resource "aws_instance" "example" {
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = base64decode(var.private_key_base64)  // Modified this line
+      private_key = base64decode(var.private_key_base64)
       host        = self.public_ip
     }
   }
@@ -39,5 +42,15 @@ resource "aws_instance" "example" {
 
 variable "private_key_base64" {
   description = "Base64 encoded private key content"
+  type        = string
+}
+
+variable "aws_access_key_id" {
+  description = "AWS access key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS secret access key"
   type        = string
 }
