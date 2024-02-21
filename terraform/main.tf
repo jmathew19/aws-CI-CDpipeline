3e34a944131b5a938fa9c63c73936781e0f1e826
Jonathan Mathew <jonathanmathew@Jonathans-MBP.lan>
2024-02-21 11:20:59 -0500

    changed terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "example" {
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = filebase64decode(var.private_key_base64)
+      private_key = filebase64decode(var.private_key_base64)  // Modify this line
       host        = self.public_ip
     }
   }
