01ee3c7f384098cf0ea208d57402f4a7eda8c21c
Jonathan Mathew <jonathanmathew@Jonathans-MBP.lan>
2024-02-22 12:46:12 -0500

    changes to Login page

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,8 @@
-provider "aws" {
-  region     = "us-east-1"  # Specify your desired AWS region
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
-}
-
 resource "aws_instance" "example" {
+  # Use the instance ID of the existing EC2 instance
+  instance_id = "i-1234567890abcdef0"
+
+  # Other attributes remain the same
   ami           = "ami-0e731c8a588258d0d" 
   instance_type = "t2.micro"
   key_name      = "terraform-key-pairs"
@@ -13,25 +11,18 @@ resource "aws_instance" "example" {
     Name = "react proj"
   }
 
-  # No count parameter, so only one instance will be managed by Terraform
+  # Lifecycle configuration to prevent recreating the instance
+  lifecycle {
+    prevent_destroy = true
+  }
 
+  # Provisioner block for updating software
   provisioner "remote-exec" {
     inline = [
-      "sudo yum update -y",
-      "mkdir project",
-      "cd project",
-      "sudo yum install git -y",
-      "git clone https://github.com/jmathew19/test.git",
-      "cd react-aws-terraform-project",
-      "sudo yum install -y nodejs npm",
-      "node --version",
-      "npm --version",
-      "npm install web-vitals",
-      "npm install react-scripts --save-dev",
-      "npm install react-dom",
-      # Additional commands to start your application
+      # Update software or configurations here
     ]
     
+    # Connection details remain the same
     connection {
       type        = "ssh"
       user        = "ec2-user"
@@ -40,6 +31,7 @@ resource "aws_instance" "example" {
     }
   }
 }
+
 
 variable "private_key_base64" {
   description = "Base64 encoded private key content"
