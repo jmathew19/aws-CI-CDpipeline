bafbaee4f7640d48d04db604630a7053e3e4678c
Jonathan Mathew <jonathanmathew@jonathans-mbp.myfiosgateway.com>
2024-02-26 13:50:33 -0500

    yml takes care of deployment

--- src/Login.js
+++ src/Login.js
@@ -45,7 +45,7 @@ function Login() {
 
   return (
     <div>
-      <h2>Login here tear:</h2>
+      <h2>Login here tear :</h2>
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Username:</label>
